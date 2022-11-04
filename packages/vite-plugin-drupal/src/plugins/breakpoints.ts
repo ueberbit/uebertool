49737e0489daaf54bfde7628a04f7f945a2fd55b
Willi Woldt <willi.woldt@ueberbit.de>
2022-11-04 17:45:05 +0100

    fix: 🐛 breakpoints generation

--- packages/vite-plugin-drupal/src/plugins/breakpoints.ts
+++ packages/vite-plugin-drupal/src/plugins/breakpoints.ts
@@ -1,6 +1,5 @@
-import fs from 'node:fs/promises'
-import { constants } from 'node:fs'
-import type { Plugin } from 'vite'
+import fse from 'fs-extra'
+import type { Plugin, ResolvedConfig } from 'vite'
 import YAML from 'yaml'
 import resolveConfig from 'tailwindcss/resolveConfig'
 
@@ -30,13 +29,12 @@ const getScreen = (theme: string, group: string, name: string, size?: number) =>
   }
 }
 
-const generateBreakpoints = async () => {
+const generateBreakpoints = async (theme: string) => {
   let screens = {}
   try {
     const tailwindConfigFile = await import(`${process.cwd()}/tailwind.config.js`)
     const tailwindConfig = resolveConfig(tailwindConfigFile)
     const breakpoints = tailwindConfig.theme?.screens ?? {} as any
-    const theme = 'UEBERBIT'
     const group = 'Tailwind'
 
     screens = {
@@ -56,48 +54,35 @@ const generateBreakpoints = async () => {
   return screens
 }
 
-export default (ctx: Context): Plugin[] => {
-  return [
-    {
-      name: 'vite-plugin-drupal-breakpoints-build',
-      apply: 'build',
-      async buildStart() {
-        const screens = await generateBreakpoints()
+export default (ctx: Context): Plugin => {
+  let config: ResolvedConfig
 
-        if (!screens)
-          return
-
-        if (!ctx.isProduction && ctx.config.build?.outDir) {
-          try {
-            await fs.access(ctx.config.build.outDir, constants.F_OK)
-          }
-          catch {
-            await fs.mkdir(ctx.config.build.outDir, { recursive: true })
-          }
-          finally {
-            await fs.writeFile(`${ctx.config.build.outDir}/ueberbit_dist.breakpoints.yml`, YAML.stringify(screens))
-          }
-        }
-      },
+  return {
+    name: 'vite-plugin-drupal-breakpoints',
+    configResolved(resolvedConfig) {
+      config = resolvedConfig
     },
-    {
-      name: 'vite-plugin-drupal-breakpoints-serve',
-      apply: 'serve',
-      async buildStart() {
-        const screens = await generateBreakpoints()
+    async generateBundle(_, bundle, isWrite) {
+      const screens = await generateBreakpoints(ctx.themeName)
+      if (!isWrite || !ctx.isProduction || !screens)
+        return
 
-        if (!screens)
-          return
-
-        if (ctx.isProduction && ctx.config.build?.outDir) {
-          this.emitFile({
-            type: 'asset',
-            name: 'ueberbit_dist.breakpoints.yml',
-            fileName: 'ueberbit_dist.breakpoints.yml',
-            source: YAML.stringify(screens),
-          })
-        }
-      },
+      if (ctx.isProduction && ctx.config.build?.outDir) {
+        this.emitFile({
+          type: 'asset',
+          name: `${ctx.distThemeName}.breakpoints.yml`,
+          fileName: `${ctx.distThemeName}.breakpoints.yml`,
+          source: YAML.stringify(screens),
+        })
+      }
     },
-  ]
+    async buildStart() {
+      const screens = await generateBreakpoints(ctx.themeName)
+
+      if (!screens || ctx.isProduction)
+        return
+
+      await fse.outputFile(`${config.build.outDir}/${ctx.distThemeName}.breakpoints.yml`, YAML.stringify(screens))
+    },
+  }
 }
