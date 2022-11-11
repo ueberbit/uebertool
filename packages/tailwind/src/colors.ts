7c341de0b1de43d55eb176f18e9fe7a90fa8fa11
Willi Woldt <willi.woldt@ueberbit.de>
2022-11-11 09:42:17 +0100

    feat: ✨ add tailwind utility package

---
+++ packages/tailwind/src/colors.ts
@@ -0,0 +1,33 @@
+type colorModel = 'rgb' | 'hsl'
+
+/**
+ * Wrap custom property with tailwind opacity value.
+ * @param {string} value - color value.
+ * @param {string} model - color model.
+ * @returns {string} a custom property.
+ */
+export const color = (value: string, model: colorModel) => {
+  return ({ opacityValue }: { opacityValue: any }) => {
+    return opacityValue !== undefined ? `${model}(var(${value}) / ${opacityValue})` : `${model}(var(${value}))`
+  }
+}
+
+/**
+ * Generate semantic color values.
+ * @param {string[]} names Array of names. E.g. primary, secondary.
+ * @param {string[]} steps Array of steps. E.g. light, dark.
+ * @returns Tailwind Color Object
+ */
+export const semanticColors = (names: string[], steps: string[], model: colorModel) => {
+  return names.reduce((a, c) => {
+    return {
+      ...a,
+      [c]: steps.reduce((aa, cc) => {
+        return {
+          ...aa,
+          [cc]: color(`--c-${c}${cc === 'DEFAULT' ? '' : `-${cc}`}`, model),
+        }
+      }, {}),
+    }
+  }, {})
+}
---
+++ packages/tailwind/src/remCalc.ts
@@ -0,0 +1,12 @@
+/**
+ * Transforms pixel value to rem.
+ * @param {number} px - Pixel value which for transformation.
+ * @param {number} base - Base pixel value.
+ * @param {number} precision - Precision of decimal value.
+ * @returns {string} Pixel value relative to base.
+ */
+export const remCalc = (px: number, base = 16, precision = 3) => {
+  return `${(px / base)
+    .toFixed(precision)
+    .replace(/\.?0*$/, '')}rem`
+}
