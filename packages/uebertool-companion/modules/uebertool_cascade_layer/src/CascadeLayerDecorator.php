107d833037d579d1ee81602c684d1a45cb564802
Willi Woldt <willi.woldt@ueberbit.de>
2023-09-06 12:57:16 +0200

    feat: ✨ update asset optimizer decorator

--- packages/uebertool-companion/modules/uebertool_cascade_layer/src/CascadeLayerDecorator.php
+++ packages/uebertool-companion/modules/uebertool_cascade_layer/src/CascadeLayerDecorator.php
@@ -2,22 +2,32 @@
 
 namespace Drupal\uebertool_cascade_layer;
 
-use Drupal\Core\Asset\CssOptimizer;
+use Drupal\Core\Asset\AssetOptimizerInterface;
 
 /**
  * Optimizes a CSS asset.
  */
-class CascadeLayerDecorator extends CssOptimizer {
+class CascadeLayerDecorator implements AssetOptimizerInterface {
+
+  public function __construct(protected AssetOptimizerInterface $prototype) {}
+
   /**
    * {@inheritdoc}
    */
   public function optimize(array $css_asset) {
-    $content = parent::optimize($css_asset);
+    $content = $this->prototype->optimize($css_asset);
 
     // Add the cascade layer to the CSS.
     $content = "@layer drupal {" . $content . "}";
 
     return $content;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function clean($content) {
+    return $this->prototype->clean($content);
   }
 
 }
