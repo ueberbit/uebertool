2a003f52d78bcbf2a218a6773e9f7ec8b5fc9de1
Willi Woldt <willi.woldt@ueberbit.de>
2024-01-19 14:59:47 +0100

    feat: ✨ allow @public alias in css (without url())

--- packages/vite-plugin-drupal/src/plugins/css.ts
+++ packages/vite-plugin-drupal/src/plugins/css.ts
@@ -37,6 +37,20 @@ export default (ctx: Context): Plugin[] => {
             }
           },
         },
+        {
+          name: 'vite-plugin-uebertool-public-path-transform',
+          transform(src, id) {
+            if (/\.css/.test(id)) {
+              const s = new MagicString(src)
+              s.replace(/(?<!url\(('|")?)(@|~)\/?public/g, `${ctx.themeBasePath}/public`)
+
+              return {
+                code: s.toString(),
+                map: null,
+              }
+            }
+          },
+        },
       ]
     : []
 }
