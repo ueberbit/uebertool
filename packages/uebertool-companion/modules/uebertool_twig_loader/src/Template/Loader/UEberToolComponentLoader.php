cdaf7e483104f2e340c475e0ece9e2ea1290b60c
Willi Woldt <willi.woldt@ueberbit.de>
2024-07-18 10:31:14 +0200

    fix: 🐛 decorate core sdc, fix sdc library name

--- packages/uebertool-companion/modules/uebertool_twig_loader/src/Template/Loader/UEberToolComponentLoader.php
+++ packages/uebertool-companion/modules/uebertool_twig_loader/src/Template/Loader/UEberToolComponentLoader.php
@@ -3,7 +3,6 @@
 namespace Drupal\uebertool_twig_loader\Template\Loader;
 
 use Drupal\Core\Asset\LibraryDiscoveryInterface;
-use Drupal\Core\Theme\ThemeManagerInterface;
 use Drupal\sdc\ComponentPluginManager;
 use Drupal\sdc\Exception\ComponentNotFoundException;
 use Drupal\sdc\Twig\TwigComponentLoader;
@@ -14,37 +13,12 @@ use Twig\Loader\LoaderInterface;
  * Lets you load templates using the component ID.
  */
 class UEberToolComponentLoader implements LoaderInterface {
-  /**
-   * The active theme.
-   *
-   * @var \Drupal\Core\Theme\ActiveTheme
-   */
-  protected $activeTheme;
-
-  /**
-   * The active theme name.
-   *
-   * @var string
-   */
-  protected $activeThemeName;
-
-  /**
-   * The active theme name with _dist suffix.
-   *
-   * @var string
-   */
-  protected $distThemeName;
 
   public function __construct(
     protected TwigComponentLoader $prototype,
     protected ComponentPluginManager $pluginManager,
-    protected ThemeManagerInterface $themeManager,
     protected LibraryDiscoveryInterface $libraryDiscovery,
-  ) {
-    $this->activeTheme = $this->themeManager->getActiveTheme();
-    $this->activeThemeName = $this->activeTheme->getName();
-    $this->distThemeName = "{$this->activeThemeName}_dist";
-  }
+  ) {}
 
   /**
    * {@inheritdoc}
@@ -53,6 +27,8 @@ class UEberToolComponentLoader implements LoaderInterface {
     try {
       $component = $this->pluginManager->find($name);
       $path = $component->getTemplatePath();
+      $themeName = explode($component::DERIVATIVE_SEPARATOR, $component->getPluginId())[0];
+      $distThemeName = "{$themeName}_dist";
     }
     catch (ComponentNotFoundException $e) {
       return new Source('', $name, '');
@@ -61,11 +37,11 @@ class UEberToolComponentLoader implements LoaderInterface {
 
     $prefix = '';
     if (array_key_exists($this->distThemeName, $this->activeTheme->getBaseThemeExtensions())) {
-      $componentName = explode(':', $name)[1];
-      $libraryName = "{$this->distThemeName}/sdc--{$componentName}";
+      $componentName = $component->getDerivativeId();
+      $libraryName = "sdc--{$componentName}";
 
-      $prefix = $this->libraryDiscovery->getLibraryByName($this->distThemeName, $libraryName)
-        ? "{{ attach_library('{$this->distThemeName}/{$libraryName}') }}"
+      $prefix = $this->libraryDiscovery->getLibraryByName($distThemeName, $libraryName)
+        ? "{{ attach_library('{$distThemeName}/{$libraryName}') }}"
         : '';
     }
 
