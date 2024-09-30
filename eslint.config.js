2d7f302863f0735d527b84b443cf958bc2018c18
Willi Woldt <willi.woldt@ueberbit.de>
2024-09-30 09:49:09 +0200

    feat: ✨ update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,11 @@
 import config from '@ueberbit/eslint-config'
 
-export default config()
+export default config({
+  ignores: [
+    'packages/tailwind/README.md',
+    'packages/vite-plugin-drupal/README.md',
+    'packages/vite-plugin-drupal/types/drupal.d.ts',
+    'packages/drupal-starterkit-theme/**',
+    'packages/uebertool-companion/**/*.yml',
+  ],
+})
